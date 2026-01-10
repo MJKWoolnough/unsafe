04c7eee25b9ad99a19d4508b02e34bd452fc159a
Michael Woolnough <michael.woolnough@gmail.com>
2026-01-10 19:27:42 +0000

    added helper func

--- write.go
+++ write.go
@@ -13,6 +13,14 @@ func WriteType(w io.Writer, pkg *types.Package, types ...string) error {
 	return nil
 }
 
+func getImports(pkg *types.Package) map[string]*types.Package {
+	imps := map[string]*types.Package{pkg.Path(): pkg}
+
+	getAllImports(pkg.Imports(), imps)
+
+	return imps
+}
+
 func getAllImports(imports []*types.Package, imps map[string]*types.Package) {
 	for _, imp := range imports {
 		if _, ok := imps[imp.Path()]; ok {
