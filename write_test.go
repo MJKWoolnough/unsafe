da0cb190314d1882499d83798ca5c892bc4014c8
Michael Woolnough <michael.woolnough@gmail.com>
2026-01-10 19:23:41 +0000

    added test for getAllStructs

--- write_test.go
+++ write_test.go
@@ -20,11 +20,39 @@ func TestGetAllImports(t *testing.T) {
 	for path, name := range map[string]string{
 		"vimagination.zapto.org/unsafe":       "main",
 		"vimagination.zapto.org/httpreaderat": "httpreaderat",
+		"vimagination.zapto.org/cache":        "cache",
 		"archive/zip":                         "zip",
 		"bytes":                               "bytes",
 	} {
 		if n := imps[path].Name(); n != name {
 			t.Errorf("expecting package %q to have name %q, got %q", path, name, n)
+		}
+	}
+}
+
+func TestGetAllStructs(t *testing.T) {
+	pkg, err := gotypes.ParsePackage(".")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	imps := map[string]*types.Package{pkg.Path(): pkg}
+
+	getAllImports(pkg.Imports(), imps)
+
+	structs := make(map[string]*types.Struct)
+
+	if err := getAllStructs(imps, structs, "vimagination.zapto.org/httpreaderat.Request"); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	for typeName, structStr := range map[string]string{
+		"vimagination.zapto.org/httpreaderat.Request": "struct{url string; length int64; blockSize int64; cache *vimagination.zapto.org/cache.LRU[int64, string]}",
+		"vimagination.zapto.org/cache.LRU":            "struct{vimagination.zapto.org/cache.cache[T, U, vimagination.zapto.org/cache.lru[T, U]]}",
+		"sync.Mutex":                                  "struct{_ sync.noCopy; mu internal/sync.Mutex}",
+	} {
+		if s := structs[typeName].String(); s != structStr {
+			t.Errorf("expecting struct %q to have type %q, got %q", typeName, structStr, s)
 		}
 	}
 }
