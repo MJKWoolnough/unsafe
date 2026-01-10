f962e39af1e0ac8784d9fdd98e7b90cea5c3b3b5
Michael Woolnough <michael.woolnough@gmail.com>
2026-01-10 17:51:07 +0000

    added func to get full map of imports

--- write.go
+++ write.go
@@ -8,3 +8,15 @@ import (
 func WriteType(w io.Writer, pkg *types.Package, types ...string) error {
 	return nil
 }
+
+func getAllImports(imports []*types.Package, imps map[string]string) {
+	for _, imp := range imports {
+		if _, ok := imps[imp.Path()]; ok {
+			continue
+		}
+
+		imps[imp.Path()] = imp.Name()
+
+		getAllImports(imp.Imports(), imps)
+	}
+}
