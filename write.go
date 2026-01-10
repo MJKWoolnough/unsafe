a7aa235d9620bc772306b02f919e9ac1f8104f03
Michael Woolnough <michael.woolnough@gmail.com>
2026-01-10 19:23:21 +0000

    added func to recursively get all required structs

--- write.go
+++ write.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"cmp"
+	"errors"
+	"fmt"
 	"go/types"
 	"io"
+	"strings"
 )
 
 func WriteType(w io.Writer, pkg *types.Package, types ...string) error {
@@ -20,3 +24,64 @@ func getAllImports(imports []*types.Package, imps map[string]*types.Package) {
 		getAllImports(imp.Imports(), imps)
 	}
 }
+
+func getAllStructs(imps map[string]*types.Package, structs map[string]*types.Struct, typename string) error {
+	genPos := strings.IndexByte(typename, '[')
+	if genPos == -1 {
+		genPos = len(typename)
+	}
+
+	if _, ok := structs[typename[:genPos]]; ok {
+		return nil
+	}
+
+	pos := strings.LastIndexByte(typename[:genPos], '.')
+	if pos < 0 {
+		return fmt.Errorf("%w: %s", ErrNoModuleType, typename)
+	}
+
+	pkg, ok := imps[typename[:pos]]
+	if !ok {
+		return fmt.Errorf("%w: %s", ErrNoModule, typename)
+	}
+
+	obj := pkg.Scope().Lookup(typename[pos+1 : genPos])
+	if obj == nil {
+		return fmt.Errorf("%w: %s", ErrNoType, typename)
+	}
+
+	if s, ok := obj.Type().Underlying().(*types.Struct); ok {
+		structs[typename[:genPos]] = s
+
+		for field := range s.Fields() {
+			if err := processField(imps, structs, field.Type()); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func processField(imps map[string]*types.Package, structs map[string]*types.Struct, field types.Type) error {
+	switch t := field.Underlying().(type) {
+	case *types.Pointer:
+		return processField(imps, structs, t.Elem())
+	case *types.Map:
+		return cmp.Or(processField(imps, structs, t.Key()), processField(imps, structs, t.Elem()))
+	case *types.Array:
+		return processField(imps, structs, t.Elem())
+	case *types.Slice:
+		return processField(imps, structs, t.Elem())
+	case *types.Struct:
+		return getAllStructs(imps, structs, field.String())
+	}
+
+	return nil
+}
+
+var (
+	ErrNoModuleType = errors.New("module-less type")
+	ErrNoModule     = errors.New("module not imported")
+	ErrNoType       = errors.New("no type found")
+)
