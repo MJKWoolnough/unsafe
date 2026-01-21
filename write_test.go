7a4ad7b9c3484f46c723a7090c843b10f945a974
Michael Woolnough <michael.woolnough@gmail.com>
2026-01-21 19:58:07 +0000

    added additional buildFunc test

--- write_test.go
+++ write_test.go
@@ -299,6 +299,7 @@ func TestBuildFunc(t *testing.T) {
 		typ, res string
 	}{
 		{"strings.Reader", "func makestrings_Reader(x *strings.Reader) *strings_Reader {\n\treturn (*strings_Reader)(unsafe.Pointer(x))\n}"},
+		{"vimagination.zapto.org/httpreaderat.block", "func makevimagination_zapto_org_httpreaderat_block(x *httpreaderat.block) *vimagination_zapto_org_httpreaderat_block {\n\treturn (*vimagination_zapto_org_httpreaderat_block)(unsafe.Pointer(x))\n}"},
 	} {
 		var buf strings.Builder
 
