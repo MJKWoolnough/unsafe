a3a134aa8ab5e0b81f5f31dfb93effb713b841ee
Michael Woolnough <michael.woolnough@gmail.com>
2026-01-23 18:07:57 +0000

    moved func gen test code to own file

---
+++ func_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"go/format"
+	"go/token"
+	"strings"
+	"testing"
+
+	"vimagination.zapto.org/gotypes"
+)
+
+func TestBuildFunc(t *testing.T) {
+	b, err := newBuilder(".")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	imps := gotypes.Imports(b.pkg)
+	b.init()
+
+	for n, test := range [...]struct {
+		typ, res string
+	}{
+		{"strings.Reader", "func makestrings_Reader(x *strings.Reader) *strings_Reader {\n\treturn (*strings_Reader)(unsafe.Pointer(x))\n}"},
+		{"vimagination.zapto.org/httpreaderat.block", "func makevimagination_zapto_org_httpreaderat_block(x *httpreaderat.block) *vimagination_zapto_org_httpreaderat_block {\n\treturn (*vimagination_zapto_org_httpreaderat_block)(unsafe.Pointer(x))\n}"},
+	} {
+		var buf strings.Builder
+
+		str, err := b.getStruct(imps, test.typ)
+		if err != nil {
+			t.Errorf("test %d: unexpected error: %s", n+1, err)
+		} else {
+			b.genImports()
+
+			format.Node(&buf, token.NewFileSet(), b.buildFunc(str))
+
+			if str := buf.String(); str != test.res {
+				t.Errorf("test %d: expecting type %q, got %q", n+1, test.res, str)
+			}
+		}
+	}
+}
